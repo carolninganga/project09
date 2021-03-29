6a209cb959065d7e753d45787f7adf0c2160e68d
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2021-03-29 16:49:35 -0400

    added shapes class

---
+++ shapes.py
@@ -0,0 +1,24 @@
+# Caroline Ninganga
+# Project09 
+# Version 3 
+# 03/29/2021
+
+# class called shapes 
+class Shapes:
+
+    def __init__(self, distance = 100, angle = 90, color = (0, 0, 0), istring = '' ):
+        
+        # create a field of self called distance and assign it distance
+        self.distance = distance
+        # create a field of self called angle and assign it angle
+        self.angle = angle
+        # create a field of self called color and assign it color
+        self.color = color
+        # create a field of self called string and assign it istring
+        self.string = istring
+
+    #setColor(self, c) - set the color field to c
+    def setColor(self, c):
+    #setDistance(self, d) - set the distance field to d
+    #setAngle(self, a) - set the angle field to a
+    #setString(self, s) - set the string field to s
